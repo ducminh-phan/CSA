595d0815392a1518d56f2de94745013b8ad7d572
Duc-Minh Phan <alephvn@gmail.com>
2018-08-12 20:55:38 +0200

    Simplify CLI implementation by using Clara, add option for unrestricted walking with hub labelling

--- csa/main.cpp
+++ csa/main.cpp
@@ -1,41 +1,28 @@
 #include <iostream>
 
+#include "clara.hpp"
 #include "data_structure.hpp"
-#include "csa.hpp"
-#include <experiments.hpp>
-
-
-void usage_exit(char* argv[]) {
-    auto paragraph = [](std::string s, size_t width = 80) -> std::string {
-        std::string acc;
-        while (!s.empty()) {
-            size_t pos = s.size();
-            if (pos > width) pos = s.rfind(' ', width);
-            std::string line = s.substr(0, pos);
-            acc += line + "\n";
-            s = s.substr(pos);
-
-            // Remove the space starting the line
-            if (s[0] == ' ') s = s.substr(1);
-        }
-        return acc;
-    };
-
-    std::cerr << "Usage: " << argv[0] << " name\n\n"
-              << paragraph("Run the Connection Scan algorithm with the generated queries and log "
-                           "the running time and arrival times of each query.\n")
-              << "Positional arguments:\n"
-              << " name\tThe name of the dataset to be used in the algorithm\n";
-    exit(1);
-}
+#include "experiments.hpp"
 
 
 int main(int argc, char* argv[]) {
-    if (argc != 2) {
-        usage_exit(argv);
-    }
+    std::string name;
+    bool use_hl = false;
+    bool show_help = false;
+    auto cli_parser = clara::Arg(name, "name")("The name of the dataset to be used in the algorithm") |
+                      clara::Opt(use_hl)["--hl"]("Unrestricted walking with hub labelling") |
+                      clara::Help(show_help);
 
-    std::string name {argv[1]};
+    auto result = cli_parser.parse(clara::Args(argc, argv));
+    if (!result) {
+        std::cerr << "Error in command line: " << result.errorMessage() << std::endl;
+        cli_parser.writeToStream(std::cout);
+        exit(1);
+    }
+    if (show_help) {
+        cli_parser.writeToStream(std::cout);
+        return 0;
+    }
 
     const Timetable timetable {name};
     timetable.summary();
